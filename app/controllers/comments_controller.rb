fdce76abeac984ca38f331165d02b69c212ec8cb
Mark Schipper <schipperius@ymail.com>
2011-03-12 16:56:14 -0700

    clean up

--- app/controllers/comments_controller.rb
+++
@@ -1,7 +0,0 @@
-class CommentsController < ApplicationController
-  def create
-    @page = Page.find(params[:page_id])
-    @comment = @page.comments.create(params[:comment])
-    redirect_to page_path(@page)
-  end
-end
